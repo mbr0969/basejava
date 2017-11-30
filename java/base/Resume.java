dabceebb55dc3e43b80d17d14bee46d83186dde0
papa <mbr0969@gmail.com>
2017-11-30 19:20:45 +0300

    Созданно 30.11.2017

---
+++ java/base/Resume.java
@@ -0,0 +1,12 @@
+package base;
+
+import java.util.Map;
+
+public class Resume {
+    private String uuid;
+    private String fullName;
+    private String location= "";
+    private String hamePage = "";
+    Map<String,String> contact;
+
+}
